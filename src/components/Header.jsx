9caab701c3e55223fe36b3876246219e127dd9f3
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-13 12:48:08 +0200

    changed content of Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@
 export default function Header() {
   return (
     <header>
-      <h1 className="text-3xl font-bold underline text-red-400">The Quizer APP to check your knowledge</h1>
+      <h1 className="text-3xl font-bold underline text-red-400">The Quizer APP</h1>
     </header>
   );
 }

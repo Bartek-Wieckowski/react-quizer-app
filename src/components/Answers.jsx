951eb75b181465f9fe39076abe87eca8ad5306b0
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-13 15:40:18 +0200

    added test logic to display correct/incorrect answer

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,15 +1,26 @@
 import PropTypes from "prop-types";
 Answers.propTypes = {
   question: PropTypes.object,
+  dispatch: PropTypes.func,
+  answer: PropTypes.string,
 };
 
-export default function Answers({ question }) {
+export default function Answers({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
   return (
     <div className="flex flex-col justify-center">
-      {Object.values(question.answers).map((value) => (
+      {Object.entries(question.answers).map(([key, value]) => (
         <button
           key={value}
-          className="max-w-md text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 hover:ml-4 focus:ring-4 focus:outline-none focus:ring-purple-300  focus:bg-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900 block"
+          onClick={() => dispatch({ type: "newAnswer", payload: key })}
+          className={`max-w-md text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 hover:ml-4 focus:ring-4 focus:outline-none focus:ring-purple-300  focus:bg-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900 block ${
+            hasAnswered
+              ? question.correct_answer === answer && question.correct_answer === key
+                ? "bg-teal-400 dark:text-black dark:hover:bg-transparent pointer-events-none"
+                : "bg-rose-400 dark:hover:bg-transparent pointer-events-none opacity-50"
+              : ""
+          }`}
+          disabled={hasAnswered}
         >
           {value}
         </button>

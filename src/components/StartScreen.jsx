8682bef42bd1830a22109c3626b31e9cc32d208d
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-13 13:11:11 +0200

    created Question comp. added start action in reducer, and dispatch this action after btn click in StartScreen

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,14 +1,18 @@
 import PropTypes from "prop-types";
 StartScreen.propTypes = {
   questionsNum: PropTypes.number.isRequired,
+  dispatch: PropTypes.func,
 };
 
-export default function StartScreen({ questionsNum }) {
+export default function StartScreen({ questionsNum, dispatch }) {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <h2 className="text-indigo-400 text-2xl">check your knowledge</h2>
       <h3 className="text-indigo-400 text-md">{questionsNum} question to test you!</h3>
-      <button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+      <button
+        className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+        onClick={() => dispatch({ type: "start" })}
+      >
         Let`s start
       </button>
     </div>

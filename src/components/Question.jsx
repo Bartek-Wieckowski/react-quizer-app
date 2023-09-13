607ec7a1b514da8c094ed7cbd82863e155e4ece0
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-13 15:39:36 +0200

    pass props

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,13 +2,15 @@ import PropTypes from "prop-types";
 import Answers from "./Answers";
 Question.propTypes = {
   question: PropTypes.object,
+  dispatch: PropTypes.func,
+  answer: PropTypes.string,
 };
 
-export default function Question({ question }) {
+export default function Question({ question, dispatch, answer }) {
   return (
     <div>
       <h4>{question.question}</h4>
-      <Answers question={question} />
+      <Answers question={question} dispatch={dispatch} answer={answer} />
     </div>
   );
 }

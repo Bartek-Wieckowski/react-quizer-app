da50dbe2dcc8b58cf06da55824af0f55584e3fd5
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-14 16:03:27 +0200

    css changes

--- src/components/NextButton.jsx
+++ src/components/NextButton.jsx
@@ -11,7 +11,7 @@ export default function NextButton({ dispatch, answer, index, questionsNum }) {
   if (index < questionsNum - 1)
     return (
       <button
-        className="block ml-auto text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900"
+        className="block ml-auto text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2  dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900"
         onClick={() => dispatch({ type: "nextQuestion" })}
       >
         Next
@@ -20,7 +20,7 @@ export default function NextButton({ dispatch, answer, index, questionsNum }) {
   if (index === questionsNum - 1)
     return (
       <button
-        className="block ml-auto text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900"
+        className="block ml-auto text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2  dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900"
         onClick={() => dispatch({ type: "finish" })}
       >
         Finish

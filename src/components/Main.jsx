6401831c17f704f3d4ec44ab8c2a6ff38a46525b
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-14 13:06:09 +0200

    small css changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,6 @@ Main.propTypes = {
 };
 export default function Main({ children }) {
   return (
-    <main className="bg-slate-600 min-h-screen flex flex-col justify-center items-center">{children}</main>
+    <main className="bg-slate-600 min-h-screen flex flex-col justify-center items-center gap-[40px]">{children}</main>
   );
 }
